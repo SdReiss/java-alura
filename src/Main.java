f43af9a50d22e437fbe899ec04813abfa4fa54f7
Breno Reis <breu1234@gmail.com>
2023-11-30 23:11:55 -0300

    feat: second lesson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is Screen Match");
+        System.out.println("Movie: Scott Pilgrim Vs. The World");
+
+        int releaseYear = 2010;
+        System.out.println("Release Year: " + releaseYear);
+        boolean includedInThePlan = true;
+        double rating = 7.5;
+
+        double averageRating = (8.6 + 7.5 + 9.2) / 3;
+        System.out.println(averageRating);
+
+        String synopsis = """
+                        A guy has to fight the seven evil exes of the girl he want to date
+                        """ + releaseYear;
+        System.out.println(synopsis);
+
+        int classification = (int) (averageRating / 2);
+        System.out.println(classification);
     }
 }
